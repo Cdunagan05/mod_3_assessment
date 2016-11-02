a828b556750bfaf5db1177516130f4e933c60871
Chase Dunagan <cdunagan@utexas.edu>
2016-11-02 12:03:33 -0600

    add search bar, view, but all hashes

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -2,8 +2,6 @@ class SearchController < ApplicationController
   def index
 
     response = Faraday.get "https://api.bestbuy.com/v1/stores((area(80202,25)))?apiKey=#{ENV['API_KEY']}&show=city,location,longName,phone,storeId,storeType&pageSize=20&format=json"
-    response_json = JSON.parse(response.body)
-    binding.pry
-
+    @response_json = JSON.parse(response.body)
   end
 end
