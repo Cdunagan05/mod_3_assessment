1b6564b7e2a480efc7bb7483019b10b2b6fede5b
Chase Dunagan <cdunagan@utexas.edu>
2016-11-02 11:19:00 -0600

    add feature test for best_buy api

--- spec/features/user_sees_15_closest_stores_spec.rb
+++ spec/features/user_sees_15_closest_stores_spec.rb
@@ -6,12 +6,19 @@ describe "When search by zipcode" do
     # When I visit "/"
     visit "/"
 
-    fill_in 
     # And I fill in a search box with "80202" and click "search"
+    fill_in "search", with: "80202"
+    click_on "search"
+
     # Then my current path should be "/search" (ignoring params)
+    expect(current_path).to eq("/search")
+
     # And I should see stores within 25 miles of 80202
     # And I should see a message that says "17 Total Stores"
+    expect(page).to have_content("17 Total Stores")
     # And I should see exactly 15 results
+    expect(results.count).to eq(15)
     # And I should see the long name, city, distance, phone number and store type for each of the 15 results
+    expect(page).to have_content("")
   end
 end
