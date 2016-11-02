2da5aca23766286cb888f3c5dcab0b339940b119
Chase Dunagan <cdunagan@utexas.edu>
2016-11-02 11:49:13 -0600

    add faraday to search controller

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,8 +1,9 @@
 class SearchController < ApplicationController
   def index
 
-    response = Faraday.get "api_key=#{ENV['API_KEY']}"
+    response = Faraday.get "https://api.bestbuy.com/v1/stores((area(80202,25)))?apiKey=#{ENV['API_KEY']}&show=city,location,longName,phone,storeId,storeType&pageSize=20&format=json"
     response_json = JSON.parse(response.body)
+    binding.pry
 
   end
 end
